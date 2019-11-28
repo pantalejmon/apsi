6d7da5672a6833b5e24d47162ea42a636a025f4e
Jan Jakubik <janekjak@gmail.com>
2019-11-28 20:07:03 +0100

    change credential file

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,6 +1,6 @@
 export class Const {
     private static port: any = process.env.PORT || 8080;
-    private static _credentialPath = "./ormconfig.json";
+    private static _credentialPath = "./src/config/Credentials.ts";
 
     public static get credentialPath() {
         return Const._credentialPath;
--- src/database/DatabaseController.ts
+++ src/database/DatabaseController.ts
@@ -1,10 +1,26 @@
 import { createConnection, Connection } from 'typeorm';
 import 'reflect-metadata';
 import { User } from './entity/User';
+import Credentials from '../config/Credentials';
 export default class DatabaseController {
     private connection: Connection
     constructor() {
-        createConnection()
+        createConnection({
+            type: "postgres",
+            host: Credentials.dbHost,
+            port: Credentials.dbPort,
+            username: Credentials.dbUsername,
+            password: Credentials.dbPassword,
+            database: Credentials.dbname,
+            entities: [
+                "./dist/database/**/*.js"
+            ],
+            extra: {
+                ssl: true
+            },
+            synchronize: true,
+            logging: true
+        })
             .then((connection) => {
                 this.connection = connection
                 const test: User = new User();
