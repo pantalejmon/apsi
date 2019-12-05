c684668c6bc965d53602e2d026c0dd51f46cb686
Jan Jakubik <janekjak@gmail.com>
2019-12-05 21:56:27 +0100

    Added http error handling

--- src/api/routes/Router.ts
+++ src/api/routes/Router.ts
@@ -4,6 +4,7 @@ import DatabaseController from '../../database/DatabaseController';
 import AuthenticationController from './security/AuthenticationController';
 import UserController from './controllers/UserController';
 import AuthorisationController from './security/AuthorisationController';
+import { Request, Response } from 'express-serve-static-core';
 
 export default class Router {
     private router: express.Application | undefined;
@@ -23,15 +24,29 @@ export default class Router {
     }
 
     private createApi() {
+        /**
+         * Logika
+         */
         this.router.post(this.api + "login", this.authController.checkLoginAndPass, this.userController.login);
         this.router.post(this.api + "register", this.userController.signUp);
 
         /**
-         * Restricted htmls
+         * Wystawienie publicznych htmli
          */
+        this.router.use(express.static("./src/public", { index: false, extensions: ['html'] }));
+        this.router.get('/', (req, res) => res.redirect('/index'));
 
+        /**
+         * Htmle statyczne prywatne
+         */
         this.router.use("/user", this.permController.verifyUser, this.userController.staticDashboard); // Tutaj przekierowujemy na patient albo doctor
         this.router.use("/patient", this.permController.verifyUser, express.static("../../../private/patient", { index: false, extensions: ['html'] }));
         this.router.use("/doctor", this.permController.verifyUser, express.static("../../../private/doctor", { index: false, extensions: ['html'] }));
+
+        /**
+         * Błędy
+         */
+        this.router.use((req: Request, res: Response) => res.status(404).send("Strona nie istnieje (-_-)"))
+        this.router.use((req: Request, res: Response) => res.status(500).send("Błąd który nie ma prawa wystąpić (-_-)"))
     }
 }
