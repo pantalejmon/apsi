8b2ef55ff1f101788964b57c7a4962a93189adb9
Dbanasie <dbanasie@gmail.com>
2019-12-11 20:55:01 +0100

    Initial template added

---
+++ src/public/js/init.js
@@ -0,0 +1,9 @@
+(function($){
+  $(function(){
+
+    $('.sidenav').sidenav();
+    setTimeout(function(){
+      alert('oko');
+    }, 5000)
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
