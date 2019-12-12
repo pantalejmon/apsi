1c3abd14397288d5ff90dc4ecd0c7b24fead575c
Jan Jakubik <janekjak@gmail.com>
2019-12-12 19:07:22 +0100

    clean frontend

--- src/public/js/init.js
+++ src/public/js/init.js
@@ -1,9 +1,9 @@
-(function($){
-  $(function(){
 
-    $('.sidenav').sidenav();
-    setTimeout(function(){
-      alert('oko');
-    }, 5000)
-  }); // end of document ready
-})(jQuery); // end of jQuery name space
+// Plis nie piszcie w jQuery jeśli nie trzeba!
+document.addEventListener('DOMContentLoaded', function () {
+  var elems = document.querySelectorAll('.sidenav');
+  var instances = M.Sidenav.init(elems);
+  setTimeout(function () {
+    alert('oko');
+  }, 5000)
+})
