05023c1ff41e411ab86c861b4907e87b4a14cb40
anusiula.789@gmail.com <anusiula.789@gmail.com>
2019-12-15 00:39:58 +0100

    strona info.html dodana. Do zrobienia na stronce rejestracji: użyć innego komponentu niż visibility zeby specjalizacja pojawiala sie w miejscu peselu; poprawic wyswietlanie daty urodzenia, aby działała jak 'date picker' na materialize

--- src/public/js/init.js
+++ src/public/js/init.js
@@ -7,3 +7,32 @@ document.addEventListener('DOMContentLoaded', function () {
     alert('oko');
   }, 5000)
 })
+
+var peselPanel = document.getElementById("pesel_panel");
+var dataUrodzeniaPanel = document.getElementById("data_urodzenia_panel");
+var specjalizacjaPanel = document.getElementById("specjalizacja_panel");
+
+var buttonPacjent = document.getElementById("pacjent");
+function wyswietlDlaPacjenta() {
+  
+  peselPanel.style.visibility = "visible";
+  dataUrodzeniaPanel.style.visibility = "visible";
+  specjalizacjaPanel.style.visibility = "hidden";
+}
+buttonPacjent.onclick = wyswietlDlaPacjenta;
+
+var buttonLekarz = document.getElementById("lekarz");
+function wyswietlDlaLekarza() {
+  peselPanel.style.visibility = "hidden";
+  dataUrodzeniaPanel.style.visibility = "hidden";
+  specjalizacjaPanel.style.visibility = "visible";
+  
+}
+buttonLekarz.onclick = wyswietlDlaLekarza;
+
+
+
+document.addEventListener('DOMContentLoaded', function () {
+  var elems = document.querySelectorAll('.datepicker');
+  var instances = M.Datepicker.init(elems, options);
+});
