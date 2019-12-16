3a097627035fe7b4f8dd671ae5a1e2bbaf420986
Jan Jakubik <janekjak@gmail.com>
2019-12-16 19:49:18 +0100

    add frontend template & menu button script

--- src/public/js/init.js
+++ src/public/js/init.js
@@ -3,9 +3,10 @@
 document.addEventListener('DOMContentLoaded', function () {
   var elems = document.querySelectorAll('.sidenav');
   var instances = M.Sidenav.init(elems);
-  setTimeout(function () {
-    alert('oko');
-  }, 5000)
+  // Komentuje to bo strasznie drażni xd
+  // setTimeout(function () {
+  //   alert('oko');
+  // }, 5000)
 })
 
 var peselPanel = document.getElementById("pesel_panel");
@@ -14,7 +15,7 @@ var specjalizacjaPanel = document.getElementById("specjalizacja_panel");
 
 var buttonPacjent = document.getElementById("pacjent");
 function wyswietlDlaPacjenta() {
-  
+
   peselPanel.style.display = "inline";
   dataUrodzeniaPanel.style.display = "inline";
   specjalizacjaPanel.style.display = "none";
@@ -26,7 +27,7 @@ function wyswietlDlaLekarza() {
   peselPanel.style.display = "none";
   dataUrodzeniaPanel.style.display = "none";
   specjalizacjaPanel.style.display = "inline";
-  
+
 }
 buttonLekarz.onclick = wyswietlDlaLekarza;
 
@@ -36,3 +37,15 @@ document.addEventListener('DOMContentLoaded', function () {
   var elems = document.querySelectorAll('.datepicker');
   var instances = M.Datepicker.init(elems, options);
 });
+
+/**
+ * 
+ * @param {*} str 
+ */
+function changeActiveMenuItem(str) {
+  const allLi = document.getElementsByTagName("li");
+  for (let t of allLi) {
+    t.classList.remove("active");
+  }
+  document.getElementById(str).classList.add("active");
+}
