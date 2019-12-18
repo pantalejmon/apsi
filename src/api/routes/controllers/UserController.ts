1dd29fddfd2ed7007d933dadcaf25c5f86258ad3
Jan Jakubik <janekjak@gmail.com>
2019-12-18 22:19:49 +0100

    add logout method

--- src/api/routes/controllers/UserController.ts
+++ src/api/routes/controllers/UserController.ts
@@ -28,7 +28,12 @@ export default class UserController {
     }
 
     public async login(req: Request, res: Response): Promise<void> {
-        res.status(200).send({ token: req.session.token });
+        res.send({ token: req.session.token, role: req.session.role });
+    }
+
+    public async logout(req: Request, res: Response): Promise<void> {
+        req.session.destroy((err) => { if (err) console.log(err) });
+        res.redirect("/index");
     }
 
     public async getMyRole(req: Request, res: Response): Promise<void> {
