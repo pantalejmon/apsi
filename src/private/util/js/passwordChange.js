f7c934e8c196a7eb3cd32fb997a68a1bec321268
OMIsie <omisie@gmail.com>
2020-01-12 20:02:10 +0100

    Change password form

--- src/private/util/js/passwordChange.js
+++ src/private/util/js/passwordChange.js
@@ -15,8 +15,8 @@ function main() {
 function changePass() {
     let pass1 = document.getElementById('pass');
     let pass2 = document.getElementById('pass2');
-    let helper = document.getElementById('help');
 
+    // If passwords are not valid don't send them to server
     if (!validatePassword(pass1, pass2)) {
         return;
     }
@@ -44,17 +44,31 @@ function changePass() {
     xhr.send(JSON.stringify(message));
 }
 
+/**
+ * Function that checks if provided passwords are valid before they
+ * are send to server to perform password change
+ * Function @returns true if passwords are valid 
+ * @param {*} pass1 new password entered first time
+ * @param {*} pass2 new password entered second time as confirmation
+ */
 function validatePassword(pass1, pass2) {
     if (!pass1 || !pass2 || pass1.length < 8 || pass2.length < 8) {
         pass1.classList.add("invalid");
         pass1.classList.remove("valid");
-        alert("Hasło za krótkie");
+        return false;
+    } else if (pass1.value !== pass2.value) {
+        pass2.classList.add("invalid");
+        pass2.classList.remove("valid");
         return false;
     } else if (pass1.value === pass2.value) {
         pass2.classList.add("valid");
         pass2.classList.remove("invalid");
+        pass1.classList.add("valid");
+        pass1.classList.remove("invalid");
         return true;
     } else {
+        pass1.classList.add("invalid");
+        pass1.classList.remove("valid");
         pass2.classList.add("invalid");
         pass2.classList.remove("valid");
         return false;
