f1b1b1e581ad35f6615d135d46ff24546ad1c775
OMIsie <omisie@gmail.com>
2020-01-26 17:02:41 +0100

    Fetch and add events to callendar

--- src/private/doctor/js/upcomingVisitsScript.js
+++ src/private/doctor/js/upcomingVisitsScript.js
@@ -10,6 +10,7 @@ document.addEventListener('DOMContentLoaded', function () {
  */
 function main() {
     renderCalendar();
+    fetchEventsAndAddToCalendar();
 }
 
 function renderCalendar() {
@@ -22,11 +23,11 @@ function renderCalendar() {
         locale: 'pl',
         eventColor: '#03a9f4',
         events: [
-            { // this object will be "parsed" into an Event Object
-                title: 'Test Visit', // a property!
-                start: '2020-01-17T12:00:00', // a property!
-                end: '2020-01-17T14:00:00' // a property! ** see important note below about 'end' **
-            }
+            // { // this object will be "parsed" into an Event Object
+            //     title: 'Test Visit', // a property!
+            //     start: '2020-01-17T12:00:00', // a property!
+            //     end: '2020-01-17T14:00:00' // a property! ** see important note below about 'end' **
+            // }
         ],
         timeFormat: 'H(:mm)',
         displayEventTime: true,
@@ -37,14 +38,16 @@ function renderCalendar() {
     calendar.render();
 }
 
-function addEventsToCalendar() {
+function fetchEventsAndAddToCalendar() {
     // Get data from API
     const xhr = new XMLHttpRequest();
     xhr.responseType = "json";
     xhr.addEventListener('load', function () {
         if (this.status === 200) {
             // for each appointment make an Event object and add it to calendar
-           
+            const json = JSON.parse(xhr.response);
+            // Process and add events to callendar
+            addEventsToCallendar(json);
         }
 
     });
@@ -56,3 +59,59 @@ function addEventsToCalendar() {
     xhr.open("GET", "http://" + window.location.host + "/api/doctor/appointment", true);
     xhr.send();
 }
+
+/**
+ * Function makes events objects compatible with calendar configuration from passed 
+ * JSON server response and adds them to callendar instance.
+ * Callendar event is in form:
+ *  {
+        title: 'Test Visit', // a property!
+        start: '2020-01-17T12:00:00', // a property!
+        end: '2020-01-17T14:00:00' // a property! ** see important note below about 'end' **
+    }
+ * @param eventsJson is server response containting appointments as array of events
+ */
+function addEventsToCallendar(eventsJson) {
+    // Iterate through events, process and add to callendar
+    for (let i = 0; i < eventsJson.length; i++) {
+        let jsonEvent = eventsJson[i];
+        let callendarEvent = {};
+        // Construct event
+        callendarEvent.title = jsonEvent.patient;
+        callendarEvent.start = convertTimestampToCalendarDate(jsonEvent.startDate);
+        callendarEvent.end = convertTimestampToCalendarDate(jsonEvent.startDate + jsonEvent.duration);
+        // Add current event to callendar
+        calendar.addEvent(callendarEvent);
+    }
+}
+
+/**
+ * Function converts date in Unix Timestamp format to date accepted
+ * by fullcallendar configuration, pattern: yyyy-mm-ddT12:00:00
+ * @param {*} timestamp as Unix Timestamp format in seconds
+ */
+function convertTimestampToCalendarDate(timestamp) {
+    let date = new Date(timestamp * 1000).toISOString();
+    return date;
+}
+
+/*
+const testJson = [
+    {
+        id: 1,
+        startDate: 1578823200,
+        duration: 7200,
+        patient: "Jan Kowalski",
+        doctor: "Doktor Strange",
+        status: "Pending"
+    },
+    {
+        id: 2,
+        startDate: 1582823200,
+        duration: 7200,
+        patient: "Maria Kowalska",
+        doctor: "Doktor Strange",
+        status: "Pending"
+    }
+]; 
+*/
