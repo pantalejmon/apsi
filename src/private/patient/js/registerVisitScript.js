013737bcdf4781376df47f3c43e2f92b49df3a31
OMIsie <omisie@gmail.com>
2020-02-04 18:24:30 +0100

    Fix appointment, add time of visit

--- src/private/patient/js/registerVisitScript.js
+++ src/private/patient/js/registerVisitScript.js
@@ -11,6 +11,7 @@ document.addEventListener('DOMContentLoaded', function () {
 function main() {
     initializeMaterializeSelectors();
     initializeDatepickers();
+    initializeTimepickers();
     getDoctorsListAndAddToSelect();
 }
 
@@ -53,7 +54,20 @@ function getDoctorsListAndAddToSelect() {
 function registerAppointment() {
     const submittedData = {};
 
-    submittedData.startDate = convertDateToTimestamp(document.getElementById("appointment-date").value);
+    // Make a timestamp with date and time and add it to json
+    let dateTimestampInSeconds = convertDateToTimestamp(document.getElementById("appointment-date").value);
+    let timepickerValue = document.getElementById("appointment-time").value;
+    if (!timepickerValue) {
+        alert("Wybierz godzinę wizyty");
+        return;
+    }
+
+    let timeInSeconds = convertTimepickerValueToSeconds(timepickerValue);
+    if (!validateTimepickerValue(timeInSeconds)) {
+        return;
+    }
+    let dateTimeInSeconds = dateTimestampInSeconds + timeInSeconds;
+    submittedData.startDate = dateTimeInSeconds;
 
     let selectDuration = document.getElementById("select-duration");
     let appointmentDuration = selectDuration.options[selectDuration.selectedIndex].value;
@@ -108,6 +122,19 @@ function validateFormInput(inputJson) {
     return true
 }
 
+function validateTimepickerValue(timepickerValueInSeconds) {
+    // Doctor works 8-16, below are values in seconds
+    let doctorWorkStartTimeSeconds = 28800;
+    let doctorWorkEndTimeSeconds = 57600;
+
+    if (timepickerValueInSeconds < doctorWorkStartTimeSeconds ||
+        timepickerValueInSeconds > doctorWorkEndTimeSeconds) {
+        alert("Lekarz rozpoczyna wizyty w godzinach 8-16, zmień godzinę wizyty");
+        return false;
+    }
+    return true;
+}
+
 /**
  * Convert date to unix timestamp
  * @param {*} strDate - date in format: yyyy/mm/dd
@@ -115,6 +142,19 @@ function validateFormInput(inputJson) {
 function convertDateToTimestamp(strDate) {
     var dateUnix = Date.parse(strDate.split("/").reverse().join("/"));
     return dateUnix / 1000;
+}
+
+/**
+ * Function converts timepicker value to seconds
+ * @param {*} timepickerValue is time from timepicker in format 'hh:mm'
+ */
+function convertTimepickerValueToSeconds(timepickerValue) {
+    let splitTime = timepickerValue.split(":");
+    let hours = Number(splitTime[0]);
+    let minutes = Number(splitTime[1]);
+
+    let timeInSeconds = (hours * 3600) + (minutes * 60);
+    return timeInSeconds;
 }
 
 function initializeMaterializeSelectors() {
@@ -129,9 +169,18 @@ function initializeDatepickers() {
     let elems = document.querySelectorAll('.datepicker');
     let datePickerOptions = {
         format: "dd/mm/yyyy",
+        firstDay: 1,
         minDate: todayDate
     };
     let instances = M.Datepicker.init(elems, datePickerOptions);
+}
+
+function initializeTimepickers() {
+    var elems = document.querySelectorAll('.timepicker');
+    let options = {
+        twelveHour: false
+    };
+    var instances = M.Timepicker.init(elems, options);
 }
 
 function chooseProtocol() {
